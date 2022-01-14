174143116fc4a192e7c68e32c0dc7d2aab7e2e9b
Van Aarde Krynauw <vanaarde@leaping-rhino.com>
2022-01-14 09:12:00 +0200

    Fix shipping build errors.
    Backport of 5cb9ccf.

--- Source/HoudiniNiagara/Public/HoudiniPointCacheLoaderCSV.h
+++ Source/HoudiniNiagara/Public/HoudiniPointCacheLoaderCSV.h
@@ -10,16 +10,20 @@ class FHoudiniPointCacheLoaderCSV : public FHoudiniPointCacheLoader
 {
     public:
         FHoudiniPointCacheLoaderCSV(const FString& InFilePath);
-        
+
+#if WITH_EDITOR
         virtual bool LoadToAsset(UHoudiniPointCache *InAsset) override;
 
 		virtual FName GetFormatID() const override { return "HCSV"; };
+#endif
 
     protected:
+	
+#if WITH_EDITOR
     	virtual bool UpdateFromStringArray(UHoudiniPointCache *InAsset, TArray<FString>& InStringArray);
 
     	// Parses the CSV title row to update the column indexes of special values we're interested in
     	// Also look for packed vectors in the first row and update the indexes accordingly
 	    virtual bool ParseCSVTitleRow(UHoudiniPointCache *InAsset, const FString& TitleRow, const FString& FirstValueRow, bool& HasPackedVectors);
-
+#endif
 };
